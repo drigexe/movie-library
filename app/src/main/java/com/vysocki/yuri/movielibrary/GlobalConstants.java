3ca5c498c073b523941f06b8e925ab434879972e
Yuri Vysocki <drigexe@gmail.com>
2018-11-08 19:47:31 +0300

    posters in movieDetailsFragment now downloading with Glide

--- app/src/main/java/com/vysocki/yuri/movielibrary/GlobalConstants.java
+++ app/src/main/java/com/vysocki/yuri/movielibrary/GlobalConstants.java
@@ -3,5 +3,6 @@ package com.vysocki.yuri.movielibrary;
 public class GlobalConstants {
     public static final String API_KEY = "9e7c1048b0798ff35aec0955a7aeab66";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String BASE_IMAGES_URL = "http://image.tmdb.org/t/p/";
 
 }
--- app/src/main/java/com/vysocki/yuri/movielibrary/MovieDetailsFragment.java
+++ app/src/main/java/com/vysocki/yuri/movielibrary/MovieDetailsFragment.java
@@ -9,7 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import static com.vysocki.yuri.movielibrary.GlobalConstants.BASE_IMAGES_URL;
 
 public class MovieDetailsFragment extends Fragment {
 
@@ -30,6 +35,8 @@ public class MovieDetailsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_movie_details, container, false);
 
         final TextView textView = view.findViewById(R.id.detailsTextView);
+        final ImageView imageView = view.findViewById(R.id.detailsImageView);
+
         Bundle bundle = this.getArguments();
         int movieId = bundle.getInt(MOVIE_ID_TAG);
 
@@ -38,14 +45,27 @@ public class MovieDetailsFragment extends Fragment {
         movieDetailsViewModel.getMovie().observe(getViewLifecycleOwner(), new Observer<Movie>() {
             @Override
             public void onChanged(@Nullable Movie movie) {
-                String content = "";
-                content += "id: " + Integer.toString(movie.getMovieId()) + "\n";
-                content += "title: " + movie.getTitle() + "\n";
-                content += "tagline: " + movie.getTagline() + "\n";
-                content += "release Date: " + movie.getReleaseDate() + "\n";
-                content += "poster path: " + movie.getPosterPath() + "\n";
-                textView.append(content);
-                textView.setText(content);
+                if (movie != null) {
+
+                    String content = "";
+                    content += "title: " + movie.getTitle() + "\n";
+
+                    if (movie.getTagline() == null) {
+                        content += "No tagline loaded" + "\n";
+                    } else {
+                        content += "tagline: " + movie.getTagline() + "\n";
+                    }
+
+                    content += "release Date: " + movie.getReleaseDate();
+                    textView.setText(content);
+
+                    if (movie.getPosterPath() != null) {
+                        Glide.with(getContext()).load(BASE_IMAGES_URL+ "w500/" + movie.getPosterPath()).into(imageView);
+                    } else {
+                        imageView.setImageResource(R.drawable.noimage);
+                    }
+
+                }
             }
         });
 
